693305b0875e229efb8f85d32138387ff5c8dce0
Karan <Karan@LAPTOP-5UAH8H1J>
2021-03-05 15:18:33 +0530

    Implemented new query form

---
+++ Forms/New_query_for,.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HariOmImpex_LMS.Forms
+{
+    public partial class New_query_form : Form
+    {
+        public New_query_form()
+        {
+            InitializeComponent();
+        }
+    }
+}
